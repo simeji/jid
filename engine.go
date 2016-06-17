6434c47581a7c5df90cbf22bdc24f1a0b3a7c2da
simeji <simeji.net@gmail.com>
2016-06-17 23:42:47 +0900

    0.2.1
    update suggestion and auto complate behavior

--- engine.go
+++ engine.go
@@ -21,7 +21,8 @@ const (
 )
 
 var (
-	f *[]rune
+	f        *[]rune
+	complete *[]rune
 )
 
 type Engine struct {
@@ -93,12 +94,14 @@ func (e *Engine) render(json *simplejson.Json) bool {
 	defer termbox.Close()
 
 	f = &[]rune{}
+	complete = &[]rune{}
 
 	contents := e.prettyContents()
 	keymode := false
 
 	for {
 		e.filterJson(string(*f))
+		e.suggest()
 		if keymode {
 			contents = e.currenKeys
 		} else {
@@ -121,34 +124,22 @@ func (e *Engine) render(json *simplejson.Json) bool {
 				lki := len(kws) - 1
 				_, kws = kws[lki], kws[:lki]
 				s = strings.Join(kws, ".")
-				*f = ([]rune(s[0:len(s)]))
+				*f = []rune(s[0:len(s)])
 			case termbox.KeyBackspace, termbox.KeyBackspace2:
 				if i := len(*f) - 1; i >= 0 {
 					slice := *f
 					*f = slice[0:i]
 				}
+			case termbox.KeyTab:
+				if len(*complete) > 0 {
+					e.autoComplete()
+				} else {
+					e.suggest()
+				}
 			case termbox.KeyEnter:
 				return true
 			case 0:
 				*f = append(*f, rune(ev.Ch))
-				s := string(*f)
-				kws := strings.Split(s, ".")
-				lki := len(kws) - 1
-				lkw, kws := kws[lki], kws[:lki]
-
-				// keyword search
-				var c []int
-				re, _ := regexp.Compile("(?i)^" + lkw + ".+")
-				for i, k := range e.currenKeys {
-					if str := re.FindString(k); str != "" {
-						c = append(c, i)
-					}
-				}
-				if len(c) == 1 {
-					kws = append(kws, e.currenKeys[c[0]])
-					s = strings.Join(kws, ".")
-					*f = ([]rune(s[0:len(s)]))
-				}
 			default:
 			}
 		case termbox.EventError:
@@ -159,6 +150,54 @@ func (e *Engine) render(json *simplejson.Json) bool {
 	}
 }
 
+func (e *Engine) autoComplete() {
+	*f = append(*f, *complete...)
+	*complete = []rune("")
+}
+
+func (e *Engine) suggest() bool {
+	s := string(*f)
+	if arr, _ := e.json.Array(); arr != nil && len(arr) == 1 {
+		if s[len(s)-1:] == "." {
+			*complete = []rune("")
+			return false
+		}
+		ds := regexp.MustCompile("\\[0?\\]?$").FindString(s)
+		cs := strings.Replace("[0]", ds, "", -1)
+		*complete = []rune(cs)
+		return true
+	}
+	kws := strings.Split(s, ".")
+	lki := len(kws) - 1
+	if lki == 0 {
+		return false
+	}
+	lkw, tkws := kws[lki], kws[:lki]
+
+	var c []int
+	re, err := regexp.Compile("(?i)^" + lkw)
+	if err != nil {
+		return false
+	}
+
+	for i, k := range e.currenKeys {
+		if str := re.FindString(k); str != "" {
+			c = append(c, i)
+			lkw = str
+		}
+	}
+
+	if len(c) == 1 {
+		kw := re.ReplaceAllString(e.currenKeys[c[0]], "")
+		*complete = []rune(kw)
+		s = strings.Join(tkws, ".") + "." + lkw
+		*f = []rune(s)
+		return true
+	}
+	*complete = []rune("")
+	return false
+}
+
 func (e *Engine) prettyContents() []string {
 	s, _ := e.json.EncodePretty()
 	return strings.Split(string(s), "\n")
@@ -166,59 +205,67 @@ func (e *Engine) prettyContents() []string {
 
 func (e *Engine) filterJson(q string) {
 	json := e.orgJson
-	if len(q) > 0 {
-		keywords := strings.Split(q, ".")
-
-		if keywords[0] != "" {
-			e.json = json
-			return
-		}
-		keywords = keywords[1:]
-
-		j := json
-
-		re := regexp.MustCompile("\\[[0-9]*\\]")
-		delre := regexp.MustCompile("\\[([0-9]+)?")
-
-		lastIdx := len(keywords) - 1
-		//eachFlg := false
-		for ki, keyword := range keywords {
-			if len(keyword) == 0 {
-				break
-			}
-
-			// abc[0]
-			if keyword[len(keyword)-1:] == "]" {
-				matchIndexes := re.FindAllStringIndex(keyword, -1)
-				kw := re.ReplaceAllString(keyword, "")
-
-				tj := j.Get(kw)
-				if ki != lastIdx {
-					j = tj
-				} else if !isEmptyJson(tj) {
-					j = tj
-				}
-				lmi := len(matchIndexes) - 1
-				for idx, m := range matchIndexes {
-					i, _ := strconv.Atoi(keyword[m[0]+1 : m[1]-1])
-					if idx == lmi && m[1]-m[0] == 2 {
-						//eachFlg = true
-					} else if tj := j.GetIndex(i); !isEmptyJson(tj) {
-						j = tj
-					}
-				}
-			} else {
-				kw := delre.ReplaceAllString(keyword, "")
-				tj := j.Get(kw)
-				if ki != lastIdx {
-					j = tj
-				} else if !isEmptyJson(tj) {
-					j = tj
-				}
-			}
-		}
-		json = j
+	if len(q) < 1 {
+		e.json = json
+		e.setCurrentKeys()
+		return
 	}
+	keywords := strings.Split(q, ".")
+
+	// check start "."
+	if keywords[0] != "" {
+		e.json = json
+		return
+	}
+
+	keywords = keywords[1:]
+
+	re := regexp.MustCompile("\\[[0-9]*\\]")
+	delre := regexp.MustCompile("\\[([0-9]+)?")
+
+	j := json
+	lastIdx := len(keywords) - 1
+
+	//eachFlg := false
+	for ki, keyword := range keywords {
+		if len(keyword) == 0 {
+			if ki != lastIdx {
+				e.json = simplejson.New()
+			}
+			break
+		}
+
+		// abc[0]
+		if keyword[:1] == "[" {
+			break
+		}
+		if keyword[len(keyword)-1:] == "]" {
+			matchIndexes := re.FindAllStringIndex(keyword, -1)
+			kw := re.ReplaceAllString(keyword, "")
+
+			tj := j.Get(kw)
+			if ki != lastIdx {
+				j = tj
+			} else if !isEmptyJson(tj) {
+				j = tj
+			}
+			lmi := len(matchIndexes) - 1
+			for idx, m := range matchIndexes {
+				i, _ := strconv.Atoi(keyword[m[0]+1 : m[1]-1])
+				if idx == lmi && m[1]-m[0] == 2 {
+					//eachFlg = true
+				} else if tj := j.GetIndex(i); !isEmptyJson(tj) {
+					j = tj
+				}
+			}
+		} else {
+			kw := delre.ReplaceAllString(keyword, "")
+			if tj := j.Get(kw); !isEmptyJson(tj) {
+				j = tj
+			}
+		}
+	}
+	json = j
 	e.json = json
 	e.setCurrentKeys()
 }
@@ -250,28 +297,29 @@ func draw(rows []string) {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
 	fs := FilterPrompt + string(*f)
-	drawln(0, 0, fs)
+	cs := string(*complete)
+	drawln(0, 0, fs+cs, []([]int){[]int{len(fs), len(fs) + len(cs)}})
 	termbox.SetCursor(len(fs), 0)
 
 	for idx, row := range rows {
-		drawln(0, idx+DefaultY, row)
+		drawln(0, idx+DefaultY, row, nil)
 	}
 
 	termbox.Flush()
 }
 
-func drawln(x int, y int, str string) {
+func drawln(x int, y int, str string, matches [][]int) {
 	color := termbox.ColorDefault
 	backgroundColor := termbox.ColorDefault
 
 	var c termbox.Attribute
 	for i, s := range str {
 		c = color
-		//for _, match := range matches {
-		//if i >= match[0] && i < match[1] {
-		//c = termbox.ColorGreen
-		//}
-		//}
+		for _, match := range matches {
+			if i >= match[0] && i < match[1] {
+				c = termbox.ColorGreen
+			}
+		}
 		termbox.SetCell(x+i, y, s, c, backgroundColor)
 	}
 }
